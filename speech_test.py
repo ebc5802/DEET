decdc3b1ef3e5893f21dec0108dbb21692bd2a74
ebc5802 <edison.chen@stern.nyu.edu>
2024-05-24 23:51:56 -0400

    Latest Speech Model Test With Chinese

--- speech_test.py
+++ speech_test.py
@@ -4,9 +4,10 @@ import scipy
 processor = AutoProcessor.from_pretrained("suno/bark")
 model = BarkModel.from_pretrained("suno/bark")
 
-voice_preset = "v2/en_speaker_6"
+# voice_preset = "v2/en_speaker_6"
+voice_preset = "v2/zh_speaker_4" # Chinese Female Voice
 
-inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+inputs = processor("Hello. 你好我的名字是丹尼尔.", voice_preset=voice_preset)
 
 audio_array = model.generate(**inputs)
 audio_array = audio_array.cpu().numpy().squeeze() # Can use CPU or GPU
