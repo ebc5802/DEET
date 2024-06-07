e7e6ae0182f27c293fd4893312e30a994f4eb520
ebc5802 <edison.chen@stern.nyu.edu>
2024-06-06 22:07:05 -0400

    basic gray login screen

---
+++ flutter_application_1/lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: Column(
+          children: [
+            // logo
+
+            // welcome back message
+
+            // email textfield
+
+            // pw textfield
+
+            // login button
+
+            // register now
+          ],
+        ));
+  }
+}
